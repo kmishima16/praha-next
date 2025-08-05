c3aca059e3dfa4147ecb9389a05fc2d813422bf6
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-08-05 09:10:15 +0000

    add: fetch csr

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,12 +12,41 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
+interface Data {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+
 export default function Home() {
+
+  const [data, setData] = useState<Data | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const result = await response.json()
+      setData(result)
+    }
+
+    fetchData().catch((e) => {
+      // handle the error as needed
+      console.error('An error occurred while fetching the data: ', e)
+    })
+  }, [])
+
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <p>{data ? `title: ${data.title}` : 'Loading...'}</p>
         <Image
           className="dark:invert"
           src="/next.svg"
